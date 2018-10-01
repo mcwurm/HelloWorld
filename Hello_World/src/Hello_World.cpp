2c03368d1eb03769c9074b1dc1aff52128160d51
michael <michael@espri>
2018-10-01 17:15:53 +0200

    First Commit (local)

---
+++ Hello_World/src/Hello_World.cpp
@@ -0,0 +1,35 @@
+//============================================================================
+// Name        : Hello_World.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+class MyClass{
+private:
+	int myInt;
+
+public:
+
+	MyClass(int myInt):myInt(myInt){}
+
+	int getMyInt() const {
+		return myInt;
+	}
+
+	void setMyInt(int myInt) {
+		this->myInt = myInt;
+	}
+};
+
+int main() {
+	MyClass myClass(1);
+
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
+	std::cout << myClass.getMyInt() << std::endl;
+
+	return 0;
+}
